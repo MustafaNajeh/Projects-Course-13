1f532501cf4aae5b9edb26f2d424c956e7649f12
Mustafa Najeh <mustafapvp250@gmail.com>
2026-07-04 15:07:00 +0300

    add getitem & reverse & updateitem & Insertafter & insertatfront & insertatback & clear & isempty Mythods

--- Project 2/Project 2/Project 2/Project 2.cpp
+++ Project 2/Project 2/Project 2/Project 2.cpp
@@ -24,6 +24,35 @@ int main()
 	cout << "\n\nafter poping: \n";
 	MyQueue.Print();
 
+	cout << "\nget item (2) : " << MyQueue.GetItem(2);
+	
+	MyQueue.Reverse();
+	cout << "\n\nitems after reverse :\n";
+	MyQueue.Print();
+
+	MyQueue.UpdateItem(2, 600);
+	cout << "\n\nQueue after Update (2): \n";
+	MyQueue.Print();
+
+	MyQueue.Insertafter(2, 800);
+	cout << "\n\nQueue after add 800 after (2)\n";
+	MyQueue.Print();
+
+	MyQueue.InsertAtFront(1000);
+	cout << "\n\nQueue after add 1000 at front: \n";
+	MyQueue.Print();
+
+	MyQueue.InsertAtBack(2000);
+	cout << "\n\nQueue after add 1000 at back: \n";
+	MyQueue.Print();
+
+	MyQueue.clear();
+	cout << "\n\nQueue after clear :\n";
+	MyQueue.Print();
+
+	MyQueue.Reverse();
+	MyQueue.Print();
+
 
 	system("pause>0");
 }
--- Project 2/Project 2/Project 2/clsMyQueue.h
+++ Project 2/Project 2/Project 2/clsMyQueue.h
@@ -25,16 +25,44 @@ public :
 		MyQueue.DeleteFirstNode();
 	}
 
-	int size() {
+	T size() {
 		return MyQueue.Size();
 	}
 
-	int front() {
+	T front() {
 		return MyQueue.GetItem(0);
 	}
 
-	int back() {
+	T back() {
 		return MyQueue.GetItem(size() - 1);
+	}
+
+	T GetItem(int index){
+		return MyQueue.GetItem(index);
+	}
+
+	void Reverse() {
+		 MyQueue.Reverse();
+	}
+
+	void UpdateItem(int index , T value) {
+		MyQueue.UpdateItem(index, value);
+	}
+
+	void Insertafter(int index, T value) {
+		MyQueue.InsertAfter(index, value);
+	}
+
+	void InsertAtFront(T value) {
+		MyQueue.InsertAtBeginning(value);
+	}
+
+	void InsertAtBack(T value) {
+		MyQueue.InsertAtEnd(value);
+	}
+
+	void clear() {
+		MyQueue.Clear();
 	}
 
 };
